66705bf45e26bf88abf797ce2baed96797980ee5
dawon <lyumilove9@gmail.com>
2022-01-10 17:14:40 +0900

    미디어 쿼리 완료

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const menuBtn = document.querySelector('.menuBtn');
 const menuList = document.querySelector('.menu');
+const sci = document.querySelector('.sci');
+const slide_btn = document.querySelector('.slide_btn');
 
 menuBtn.onclick = function () {
   menuBtn.classList.toggle('clicked');
   menuList.classList.toggle('clicked');
+  sci.classList.toggle('hide');
+  slide_btn.classList.toggle('hide');
 };
